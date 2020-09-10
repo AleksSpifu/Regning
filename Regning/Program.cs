ea9c3185fa9d554863c27bf50f98510fa48c169d
Aleksander Furnes <66044119+AleksSpifu@users.noreply.github.com>
2020-09-10 21:57:40 +0200

    Add project files.

---
+++ Regning/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Regning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sju = 7;
+            int fem = 5;
+
+            fem = fem + sju;
+            sju = fem - sju;
+            fem = fem - sju;
+            Console.WriteLine("Fem har blitt " + fem + ", og sju har blitt " + sju);
+        }
+    }
+}
